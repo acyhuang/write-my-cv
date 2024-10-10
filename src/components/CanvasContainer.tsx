0fda00cb08e0551a86c203731fe5b64664994b99
Allison Huang <allisoncyhuang@gmail.com>
2024-10-10 03:38:42 -0700

    Created main divs on frontend

---
+++ src/components/CanvasContainer.tsx
@@ -0,0 +1,10 @@
+function CanvasContainer() {
+    return (
+      <div className="bg-blue-300 w-2/3 p-4 overflow-y-auto">
+        <h1>canvas-container</h1>
+        {/* Add more content here */}
+      </div>
+    );
+  }
+  
+  export default CanvasContainer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+      <header className="bg-green-300 p-4">
+        <p>write-my-cv</p>
+      </header>
+    );
+  }
+  
+  export default Header;
